f1d11da99a2103db88d5af0bf6a32ad197de0d79
Javier Segura <javier@miolabs.com>
2020-11-16 15:21:52 +0100

    Adding keypath methods for Linux

---
+++ Sources/MIOCoreData/Linux/NSManagedObject+KeyValueCoding.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Javier Segura Perez on 10/11/2020.
+//
+
+import Foundation
+
+#if os(Linux)
+
+extension NSManagedObject
+{
+    open func value(forKeyPath keyPath: String) -> Any? {
+        
+        let keys = keyPath.split(separator: ".")
+        let key = String(keys[0])
+        let value = self.value(forKey: key)
+        if keys.count == 1 { return value }
+        
+        guard let rel = entity.relationshipsByName[key] else { return nil }
+        if rel.isToMany { return nil }
+        
+        let obj = value as! NSManagedObject
+        
+        let new_keypath = keys[1...].joined(separator: ".")
+        return obj.value(forKeyPath: new_keypath)
+    }
+    
+//    open func setValue(_ value: Any?, forKey key: String) { }
+}
+
+#endif
+
