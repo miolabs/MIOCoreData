7144726b4173dc84233dbd2e26d0a91ec4be13df
Javier Segura <javier@miolabs.com>
2021-05-27 21:36:30 +0200

    Catching bugs

--- Sources/MIOCoreData/Linux/NSManagedObject+KeyValueCoding.swift
+++ Sources/MIOCoreData/Linux/NSManagedObject+KeyValueCoding.swift
@@ -14,9 +14,10 @@ extension NSManagedObject
     open func value(forKeyPath keyPath: String) -> Any? {
         
         print("value:forKeyPath: \(keyPath)")
+        print("object: \(self)")
         let keys = keyPath.split(separator: ".")
         let key = String(keys[0])
-        let value = self.value(forKey: key)
+        let value = value(forKey: key)
         print("value:forKey: \(key), value: \(value)")
         if keys.count == 1 { return value }
         
