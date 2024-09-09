8370ef62a50651cc766efc9147986b33a27ce6b1
Javier Segura <javier@miolabs.com>
2024-09-09 13:51:02 +0200

    Adding conditional compilation fro plugin builder

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 import Foundation
 
 //var swift_settings:[SwiftSetting]? = [ .define( "APPLE_CORE_DATA" ) ]
+
+let build_plugin = ( ProcessInfo.processInfo.environment["BUILD_PLUGIN"]?.lowercased() == "true" )
+
 #if os(Linux)
 let binary_target = Target.binaryTarget( name: "model-builder",
                                          url: "https://github.com/miolabs/MIOCoreData/releases/download/v1.0.5/model-builder.artifactbundle.zip",
@@ -13,53 +16,54 @@ let binary_target = Target.binaryTarget( name: "model-builder",
 let binary_target = Target.binaryTarget( name: "model-builder", path: "Binaries/model-builder.artifactbundle" )
 #endif
 
+var products:[Product] = [
+    .library(name: "CoreDataSwift", targets: ["CoreDataSwift"]),
+    .library(name: "MIOCoreData", targets: ["MIOCoreData"]),
+]
+
+var targets:[Target] = [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .executableTarget(
+        name: "ModelBuilder",
+        dependencies: [
+            .product( name: "ArgumentParser", package: "swift-argument-parser" )
+        ]
+    ),
+    .target(
+        name: "CoreDataSwift",
+        dependencies: ["MIOCore"]
+    ),
+    .target(
+        name: "MIOCoreData",
+        dependencies: ["MIOCore", "CoreDataSwift"]
+    ),
+    .testTarget(
+        name: "MIOCoreDataTests",
+        dependencies: ["MIOCoreData"]),
+    .testTarget(
+        name: "AppleCoreDataTests"
+    )
+]
+
+if build_plugin {
+    products.append( .plugin(name: "ModelBuilderPlugin", targets: ["ModelBuilderPlugin"]) )
+    targets.append( binary_target )
+    targets.append( .plugin(name: "ModelBuilderPlugin", capability: .buildTool(), dependencies: ["model-builder"]) )
+}
+
 let package = Package(
     name: "MIOCoreData",
     platforms: [
         .iOS( .v13),
         .macOS(.v12),
     ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library( name: "CoreDataSwift", targets: ["MIOCoreData"] ),
-        .library( name: "MIOCoreData", targets: ["MIOCoreData"] ),
-        .plugin( name: "ModelBuilderPlugin", targets: ["ModelBuilderPlugin"] ),
-  //      .executable( name: "ModelBuilder", targets: ["ModelBuilder"] )
-    ],
+    products: products,
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package( url: "https://github.com/miolabs/MIOCore.git", branch:"master" ),
         .package( url: "https://github.com/apple/swift-argument-parser", from: "1.5.0" ),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(
-            name: "ModelBuilder",
-            dependencies: [
-                .product( name: "ArgumentParser", package: "swift-argument-parser" )
-            ]
-        ),
-        .plugin(
-            name: "ModelBuilderPlugin",
-            capability: .buildTool(),
-            dependencies: [ "model-builder" ]
-        ),
-        binary_target,
-        .target(
-            name: "CoreDataSwift",
-            dependencies: ["MIOCore"]
-        ),
-        .target(
-            name: "MIOCoreData",
-            dependencies: ["MIOCore", "CoreDataSwift"]
-        ),
-        .testTarget(
-            name: "MIOCoreDataTests",
-            dependencies: ["MIOCoreData"]),
-        .testTarget(
-            name: "AppleCoreDataTests"
-        ),
-    ]
+    targets: targets
 )
