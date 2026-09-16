81c18840c20049df904013139014964d31a6a594
Jorge Trigger <lordchronotrigger@gmail.com>
2026-09-16 14:37:46 +0200

    iOS 15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MIOCoreData",
     platforms: [
-        .iOS( .v13),
+        .iOS( .v15),
         .macOS(.v12),
     ],
     products: [
